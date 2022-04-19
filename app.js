628b2802a11247af2819d4bf97e306ad4698b9e5
Federico Garcia <fgarcia@digitalhouse.com>
2022-04-18 22:10:21 -0300

    agregué un archivo app.js con comandos

---
+++ app.js
@@ -0,0 +1,18 @@
+// Comandos de git
+
+/*
+    git init
+    git status
+    git add
+    git commit
+    git pull
+    git config user.name --global
+    git config user.email --global
+    git push origin master
+    git remote add
+    git clone
+    git remote -v
+    git log
+    git branch -M 
+*/
+
